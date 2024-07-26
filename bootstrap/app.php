bc653895ace57cfeb22cb0a424c33d7745c86b9f
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-07-26 22:14:40 +0800

    FEATURE: Setup error handling to use response formatter trait

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,5 +14,51 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // Global render
+        $exceptions->render(function (\Symfony\Component\HttpKernel\Exception\HttpException $exception, \Illuminate\Http\Request $request) {
+            if ($request->is('api/*')) {
+
+                $statusCode = $exception?->getStatusCode() ?? $exception?->getCode() ?: 500;
+                return (new class {
+                    use \App\Traits\JsonResponseFormatter;
+                })->jsonResponse(
+                    data      : [],
+                    statusCode: $statusCode,
+                    message   : $exception->getMessage() ?: __("responses.api.{$statusCode}"),
+                );
+            }
+
+            return response($exception->getMessage(), 500);
+        });
+
+        // If exception came from AuthorizationRequests of Policy
+        $exceptions->render(
+            fn(\Illuminate\Auth\Access\AuthorizationException $exception, \Illuminate\Http\Request $request) => $request->is('api/*')
+                ? (new class {
+                    use \App\Traits\ErrorJsonResponseFormatter;
+                })->jsonForbiddenResponse(
+                    message: $exception?->getMessage() ?: __("responses.api.403"),
+                )
+                : response($exception->getMessage(), 403)
+        );
+
+        // If exception is ModelNotFoundException, use 404
+        $exceptions->render(
+            fn(\Illuminate\Database\Eloquent\ModelNotFoundException $exception, \Illuminate\Http\Request $request) => $request->is('api/*')
+                ? (new class {
+                    use \App\Traits\ErrorJsonResponseFormatter;
+                })->jsonNotFoundResponse(
+                    message: $exception->getMessage() ?: __("responses.api.404"),
+                )
+                : response($exception->getMessage(), 404)
+        );
+
+        // Global Context
+        $exceptions->context(fn(\Illuminate\Http\Request $request) => [
+            'authId'    => auth()->check() ? auth()->id() : null,
+            'payload'   => $request->toArray(),
+            'ipAddress' => $request->ip(),
+            'userAgent' => $request->userAgent(),
+            'timestamp' => now()->format('Y-m-d H:i:s'),
+        ]);
     })->create();
