debaad46a6bf341774bd8fc725dca0ae3ba1b205
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-07-28 16:22:58 +0800

    FEATURE: Create Health Check | Added middleware on logout

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('health-check', function () {
+    return response()->json([
+        'timestamp' => now()->format('Y-m-d H:i:s'),
+        'status'    => 'ok',
+    ]);
+});
+
 /**
  * Authentication Routes
  */
 Route::group([
-    'prefix' => 'auth',
+    'prefix'     => 'auth',
     'controller' => \App\Http\Controllers\AuthenticationController::class
 ], function () {
     Route::post('login', 'login')->name('api.auth.login');
-    Route::post('logout', 'logout')->name('api.auth.logout');
+    Route::post('logout', 'logout')->name('api.auth.logout')->middleware('auth:api');
     Route::post('register', 'register')->name('api.auth.register');
 });
